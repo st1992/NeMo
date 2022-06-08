f6d671d0faef43758856b716973e413a6d1fc79c
Xuesong Yang <1646669+XuesongYang@users.noreply.github.com>
2022-06-08 00:25:14 -0700

    [TTS] data processing script for HUI-ACG.

--- scripts/dataset_processing/tts/hui_acg/get_data.py
+++ scripts/dataset_processing/tts/hui_acg/get_data.py
@@ -19,9 +19,9 @@ import shutil
 import urllib.request
 from pathlib import Path
 
+from joblib import Parallel, delayed
 from nemo_text_processing.text_normalization.normalize import Normalizer
 from tqdm import tqdm
-
 
 # full corpus.
 URLS_FULL = {
@@ -50,28 +50,46 @@ URL_STATS_CLEAN = (
 
 def get_args():
     parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description="Download HUI-Audio-Corpus-German and create manifests with predefined split. "
-                    "Please check details about the corpus in https://github.com/iisys-hof/HUI-Audio-Corpus-German."
+        "Please check details about the corpus in https://github.com/iisys-hof/HUI-Audio-Corpus-German.",
     )
     parser.add_argument("--data-root", required=True, type=Path)
     parser.add_argument("--set-type", default="clean", choices=["full", "clean"], type=str)
     parser.add_argument("--min-duration", default=0.1, type=float)
     parser.add_argument("--max-duration", default=15, type=float)
-    parser.add_argument("--val-size", default=100, type=int)
-    parser.add_argument("--test-size", default=200, type=int)
+    parser.add_argument(
+        "--num-workers",
+        default=-1,
+        type=int,
+        help="Specify the max number of concurrently text normalization Python workers processes. "
+        "If -1 all CPUs are used. If 1 no parallel computing is used. This argument is only "
+        "triggered when --normalize-text is called.",
+    )
+    parser.add_argument(
+        "--normalize-text",
+        default=False,
+        action='store_true',
+        help="Normalize original text and add a new entry 'normalized_text' to .json file if True.",
+    )
+    parser.add_argument(
+        "--val-num-uttrs-per-speaker",
+        default=1,
+        type=int,
+        help="Specify the number of utterances for each speaker in val split. All speakers are covered.",
+    )
+    parser.add_argument(
+        "--test-num-utters-per-speaker",
+        default=1,
+        type=int,
+        help="Specify the number of utterances for each speaker in test split. All speakers are covered.",
+    )
     parser.add_argument(
         "--seed-for-ds-split",
         default=100,
         type=float,
         help="Seed for deterministic split of train/dev/test, NVIDIA's default is 100",
     )
-    parser.add_argument(
-        "--speaker",
-        default="Karlsson",
-        choices=["Bernd_Ungerer", "Hokuspokus", "Friedrich", "Karlsson", "Eva_K"],
-        type=str,
-    )
-
     args = parser.parse_args()
     return args
 
@@ -79,7 +97,7 @@ def get_args():
 def __maybe_download_file(source_url, destination_path):
     if not destination_path.exists():
         tmp_file_path = destination_path.with_suffix(".tmp")
-        urllib.request.urlretrieve(source_url, filename=str(tmp_file_path))
+        urllib.request.urlretrieve(source_url, filename=tmp_file_path)
         tmp_file_path.rename(destination_path)
 
 
@@ -90,105 +108,164 @@ def __extract_file(filepath, data_dir):
         print(f"Error while extracting {filepath}. Already extracted?")
 
 
+def __save_json(json_file, dict_list):
+    with open(json_file, "w") as f:
+        for d in dict_list:
+            f.write(json.dumps(d) + "\n")
+
+
 def __process_data(
-        speaker_id,
-        dataset_path,
-        stat_path,
-        min_duration,
-        max_duration,
-        val_size,
-        test_size,
-        seed_for_ds_split,
+    dataset_path,
+    stat_path,
+    speaker_id,
+    min_duration,
+    max_duration,
+    val_size,
+    test_size,
+    seed_for_ds_split,
 ):
-    # Create normalizer
-    text_normalizer = Normalizer(
-        lang="de",
-        input_case="cased",
-        overwrite_cache=True,
-        cache_dir=str(dataset_path / "cache_dir"),
-    )
-    text_normalizer_call_kwargs = {
-        "punct_pre_process": True,
-        "punct_post_process": True,
-    }
-
-    def normalizer_call(x):
-        return text_normalizer.normalize(x, **text_normalizer_call_kwargs)
-
     entries = []
-    with open(stat_path) as f:
+    with open(stat_path, 'r') as f:
         # Let's skip the header
         f.readline()
         for line in tqdm(f):
             file_stem, duration, *_, text = line.strip().split("|")
             duration = float(duration)
 
-            # file_stem -> dir_name (e.g. maerchen_01_f000051 -> maerchen, ber_psychoanalyse_01_f000046 -> ber_psychoanalyse)
+            # file_stem -> dir_name (e.g. maerchen_01_f000051 -> maerchen)
             dir_name = "_".join(file_stem.split("_")[:-2])
             audio_path = dataset_path / dir_name / "wavs" / f"{file_stem}.wav"
 
             if min_duration <= duration <= max_duration:
-                normalized_text = normalizer_call(text)
                 entry = {
                     "audio_filepath": str(audio_path),
                     "duration": duration,
                     "text": text,
-                    "normalized_text": normalized_text,
                     "speaker": speaker_id,
                 }
                 entries.append(entry)
 
     random.Random(seed_for_ds_split).shuffle(entries)
     train_size = len(entries) - val_size - test_size
-
     assert train_size > 0, "Not enough data for train, val and test"
 
-    def save(p, data):
-        with open(p, "w") as f:
-            for d in data:
-                f.write(json.dumps(d) + "\n")
+    return entries[:train_size], entries[train_size : train_size + val_size], entries[train_size + val_size :]
 
-    save(dataset_path / "train_manifest.json", entries[:train_size])
-    save(dataset_path / "val_manifest.json", entries[train_size: train_size + val_size])
-    save(dataset_path / "test_manifest.json", entries[train_size + val_size:])
+
+def __text_normalization(json_file, num_workers=-1):
+    text_normalizer_call_kwargs = {
+        "punct_pre_process": True,
+        "punct_post_process": True,
+    }
+    text_normalizer = Normalizer(
+        lang="de",
+        input_case="cased",
+        overwrite_cache=True,
+        cache_dir=str(json_file.parent / "cache_dir"),
+    )
+
+    def normalizer_call(x):
+        return text_normalizer.normalize(x, **text_normalizer_call_kwargs)
+
+    def add_normalized_text(line_dict):
+        normalized_text = normalizer_call(line_dict["text"])
+        line_dict.update({"normalized_text": normalized_text})
+        return line_dict
+
+    with open(json_file, 'r', encoding='utf-8') as fjson:
+        lines = fjson.readlines()
+        dict_list = Parallel(n_jobs=num_workers)(
+            delayed(add_normalized_text)(json.loads(line) for line in tqdm(lines))
+        )
+
+    json_file_text_normed = json_file.parent / f"{json_file.stem}_text_normed.{json_file.suffix}"
+    with open(json_file_text_normed, 'w', encoding="utf-8") as fjson_norm:
+        for dct in dict_list:
+            fjson_norm.write(json.dumps(dct) + "\n")
+    print(f"Text normalization is complete: {json_file} --> {json_file_text_normed}")
 
 
 def main():
     args = get_args()
-
-    speaker = args.speaker
+    data_root = args.data_root
     set_type = args.set_type
 
-    speaker_data_source = URLS_FULL[speaker] if set_type == "full" else URLS_CLEAN[speaker]
-    stats_source = URL_STATS_FULL if set_type == "full" else URL_STATS_CLEAN
-    dataset_root = f"{args.data_root}/HUI-Audio-Corpus-German"
+    dataset_root = data_root / f"HUI-Audio-Corpus-German-{set_type}"
     dataset_root.mkdir(parents=True, exist_ok=True)
 
-    zipped_speaker_data_path = dataset_root / Path(speaker_data_source).name
-    zipped_stats_path = dataset_root / Path(stats_source).name
+    if set_type == "full":
+        data_source = URLS_FULL
+        stats_source = URL_STATS_FULL
+    elif set_type == "clean":
+        data_source = URLS_CLEAN
+        stats_source = URL_STATS_CLEAN
+    else:
+        raise ValueError(f"Unknown {set_type}. Please choose either clean or full.")
 
-    __maybe_download_file(speaker_data_source, zipped_speaker_data_path)
-    __maybe_download_file(stats_source, zipped_stats_path)
+    # download datasets
+    speakers = []
+    for speaker in data_source:
+        speakers.append(speaker)
+        speaker_data_source = data_source[speaker]
+        zipped_speaker_data_path = dataset_root / Path(speaker_data_source).name
+        zipped_stats_path = dataset_root / Path(stats_source).name
 
-    __extract_file(zipped_speaker_data_path, dataset_root)
-    __extract_file(zipped_stats_path, dataset_root)
+        __maybe_download_file(speaker_data_source, zipped_speaker_data_path)
+        __maybe_download_file(stats_source, zipped_stats_path)
 
-    # Rename unzipped speaker data folder which has `speaker` name to `Path(speaker_data_source).stem`
-    # to avoid name conflicts between full and clean
-    speaker_data_path = dataset_root / speaker
-    speaker_data_path = speaker_data_path.rename(dataset_root / Path(speaker_data_source).stem)
+        __extract_file(zipped_speaker_data_path, dataset_root)
+        __extract_file(zipped_stats_path, dataset_root)
 
-    stats_path = dataset_root / Path(stats_source).stem / "speacker" / speaker / "overview.csv"
+    # generate json files for train/val/test splits
+    stats_path_root = dataset_root / Path(stats_source).stem / "speacker"
+    entries_train, entries_val, entries_test = [], [], []
+    speaker_to_id = {}
+    num_speakers = 0
+    for child in stats_path_root.iterdir():
+        if child.is_dir():
+            speaker = child.name
+            num_speakers += 1
+            speaker_to_id[speaker] = num_speakers
+            speaker_stats_path = stats_path_root / speaker / "overview.csv"
 
-    __process_data(
-        speaker_data_path,
-        stats_path,
-        args.min_duration,
-        args.max_duration,
-        args.val_size,
-        args.test_size,
-        args.seed_for_ds_split,
-    )
+            speaker_data_source = data_source[speaker]
+            speaker_data_path = dataset_root / Path(speaker_data_source).stem
+            if speaker not in speakers:  # others
+                speaker_data_path = speaker_data_path / speaker
+
+            train, val, test = __process_data(
+                speaker_data_path,
+                speaker_stats_path,
+                num_speakers,
+                args.min_duration,
+                args.max_duration,
+                args.val_num_uttrs_per_speaker,
+                args.test_num_uttrs_per_speaker,
+                args.seed_for_ds_split,
+            )
+
+            entries_train.extend(train)
+            entries_val.extend(val)
+            entries_test.extend(test)
+
+    # shuffle across multiple speakers
+    random.Random(args.seed_for_ds_split).shuffle(entries_train)
+    random.Random(args.seed_for_ds_split).shuffle(entries_val)
+    random.Random(args.seed_for_ds_split).shuffle(entries_test)
+
+    # save json splits.
+    train_json = dataset_root / "train_manifest.json"
+    val_json = dataset_root / "val_manifest.json"
+    test_json = dataset_root / "test_manifest.json"
+    __save_json(train_json, entries_train)
+    __save_json(val_json, entries_val)
+    __save_json(test_json, entries_test)
+
+    # normalize text if requested. New json file, train_manifest_text_normalized.json, will be generated.
+    if args.normalize_text:
+        __text_normalization(train_json, args.num_workers)
+        __text_normalization(val_json, args.num_workers)
+        __text_normalization(test_json, args.num_workers)
 
 
 if __name__ == "__main__":
